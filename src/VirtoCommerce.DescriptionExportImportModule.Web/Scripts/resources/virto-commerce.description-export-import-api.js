0f104babb726ccebf256a67bbc95a759162cd4eb
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2021-08-23 12:30:35 +0200

    Remove redundant files

--- src/VirtoCommerce.DescriptionExportImportModule.Web/Scripts/resources/virto-commerce.description-export-import-api.js
+++
@@ -1,4 +0,0 @@
-angular.module('virtoCommerce.descriptionExportImport')
-    .factory('virtoCommerce.descriptionExportImport.webApi', ['$resource', function ($resource) {
-        return $resource('api/VirtoCommerceDescriptionExportImport');
-}]);
