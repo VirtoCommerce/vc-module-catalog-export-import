db33920be042b8bb4cd63b6df9a6e94005972101
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2021-08-23 13:08:38 +0200

    Remove redundant permissions and settings

--- src/VirtoCommerce.DescriptionExportImportModule.Core/ModuleConstants.cs
+++ src/VirtoCommerce.DescriptionExportImportModule.Core/ModuleConstants.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using VirtoCommerce.Platform.Core.Settings;
 
@@ -9,13 +10,7 @@ namespace VirtoCommerce.DescriptionExportImportModule.Core
         {
             public static class Permissions
             {
-                public const string Access = "virtoCommerceDescriptionExportImport:access";
-                public const string Create = "virtoCommerceDescriptionExportImport:create";
-                public const string Read = "virtoCommerceDescriptionExportImport:read";
-                public const string Update = "virtoCommerceDescriptionExportImport:update";
-                public const string Delete = "virtoCommerceDescriptionExportImport:delete";
-
-                public static string[] AllPermissions { get; } = { Read, Create, Access, Update, Delete };
+                public static string[] AllPermissions { get; } = { };
             }
         }
 
@@ -23,28 +18,11 @@ namespace VirtoCommerce.DescriptionExportImportModule.Core
         {
             public static class General
             {
-                public static SettingDescriptor VirtoCommerceDescriptionExportImportEnabled { get; } = new SettingDescriptor
-                {
-                    Name = "VirtoCommerceDescriptionExportImport.VirtoCommerceDescriptionExportImportEnabled",
-                    GroupName = "VirtoCommerceDescriptionExportImport|General",
-                    ValueType = SettingValueType.Boolean,
-                    DefaultValue = false
-                };
-
-                public static SettingDescriptor VirtoCommerceDescriptionExportImportPassword { get; } = new SettingDescriptor
-                {
-                    Name = "VirtoCommerceDescriptionExportImport.VirtoCommerceDescriptionExportImportPassword",
-                    GroupName = "VirtoCommerceDescriptionExportImport|Advanced",
-                    ValueType = SettingValueType.SecureString,
-                    DefaultValue = "qwerty"
-                };
-
                 public static IEnumerable<SettingDescriptor> AllSettings
                 {
                     get
                     {
-                        yield return VirtoCommerceDescriptionExportImportEnabled;
-                        yield return VirtoCommerceDescriptionExportImportPassword;
+                        return Array.Empty<SettingDescriptor>();
                     }
                 }
             }
