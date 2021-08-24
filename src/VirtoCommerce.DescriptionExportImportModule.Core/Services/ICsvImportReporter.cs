fa6ac2f23fddad572d0f6b347d0bdb6197f324b5
Anton Veretennikov <superiorboroda@gmail.com>
2021-08-24 17:04:08 +0300

    Implement patch method

--- src/VirtoCommerce.DescriptionExportImportModule.Core/Services/ICsvImportReporter.cs
+++ src/VirtoCommerce.DescriptionExportImportModule.Core/Services/ICsvImportReporter.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Threading.Tasks;
+using VirtoCommerce.DescriptionExportImportModule.Core.Models;
 
 namespace VirtoCommerce.DescriptionExportImportModule.Core.Services
 {
