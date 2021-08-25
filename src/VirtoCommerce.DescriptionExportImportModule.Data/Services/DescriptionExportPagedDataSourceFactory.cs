104c3e31b8dc9a9ad3e0476af430f6d306a15ea3
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2021-08-25 12:37:46 +0200

    Add item service

--- src/VirtoCommerce.DescriptionExportImportModule.Data/Services/DescriptionExportPagedDataSourceFactory.cs
+++ src/VirtoCommerce.DescriptionExportImportModule.Data/Services/DescriptionExportPagedDataSourceFactory.cs
@@ -1,3 +1,4 @@
+using VirtoCommerce.CatalogModule.Core.Services;
 using VirtoCommerce.DescriptionExportImportModule.Core.Models;
 using VirtoCommerce.DescriptionExportImportModule.Core.Services;
 
@@ -6,15 +7,20 @@ namespace VirtoCommerce.DescriptionExportImportModule.Data.Services
     public class DescriptionExportPagedDataSourceFactory : IDescriptionExportPagedDataSourceFactory
     {
         private readonly IProductDescriptionSearchService _productDescriptionSearchService;
+        private readonly IItemService _itemService;
 
-        public DescriptionExportPagedDataSourceFactory(IProductDescriptionSearchService productDescriptionSearchService)
+        public DescriptionExportPagedDataSourceFactory(
+            IProductDescriptionSearchService productDescriptionSearchService,
+            IItemService itemService
+            )
         {
             _productDescriptionSearchService = productDescriptionSearchService;
+            _itemService = itemService;
         }
 
         public IDescriptionExportPagedDataSource Create(int pageSize, ExportDataRequest request)
         {
-            return new DescriptionExportPagedDataSource(_productDescriptionSearchService, pageSize, request);
+            return new DescriptionExportPagedDataSource(_productDescriptionSearchService, _itemService, pageSize, request);
         }
     }
 }
