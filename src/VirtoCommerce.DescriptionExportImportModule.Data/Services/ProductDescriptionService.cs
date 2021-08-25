3a7c85a839db1ba51484ba3fb85f6d6d29adf808
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2021-08-25 13:33:22 +0200

    Add caching

--- src/VirtoCommerce.DescriptionExportImportModule.Data/Services/ProductDescriptionService.cs
+++ src/VirtoCommerce.DescriptionExportImportModule.Data/Services/ProductDescriptionService.cs
@@ -2,11 +2,14 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using VirtoCommerce.CatalogModule.Core.Model;
+using VirtoCommerce.CatalogModule.Data.Caching;
 using VirtoCommerce.CatalogModule.Data.Model;
 using VirtoCommerce.CatalogModule.Data.Repositories;
 using VirtoCommerce.DescriptionExportImportModule.Core.Models;
 using VirtoCommerce.DescriptionExportImportModule.Core.Services;
+using VirtoCommerce.Platform.Core.Caching;
 using VirtoCommerce.Platform.Core.Common;
 
 namespace VirtoCommerce.DescriptionExportImportModule.Data.Services
@@ -14,25 +17,52 @@ namespace VirtoCommerce.DescriptionExportImportModule.Data.Services
     public class ProductDescriptionService : IProductDescriptionService
     {
         private readonly Func<ICatalogRepository> _catalogRepositoryFactory;
+        private readonly IPlatformMemoryCache _platformMemoryCache;
 
-        public ProductDescriptionService(Func<ICatalogRepository> catalogRepositoryFactory)
+        public ProductDescriptionService(
+            Func<ICatalogRepository> catalogRepositoryFactory,
+            IPlatformMemoryCache platformMemoryCache
+            )
         {
             _catalogRepositoryFactory = catalogRepositoryFactory;
+            _platformMemoryCache = platformMemoryCache;
         }
 
         public async Task<EditorialReview[]> GetByIdsAsync(string[] ids)
         {
-            //TODO: Add cache
-            using var catalogRepository = _catalogRepositoryFactory();
+            if (ids.IsNullOrEmpty())
+            {
+                return Array.Empty<EditorialReview>();
+            }
 
-            var descriptions = await catalogRepository
-                .EditorialReviews
-                .Where(x => ids.Contains(x.Id))
-                .ToArrayAsync();
+            var cacheKey = CacheKey.With(GetType(), nameof(GetByIdsAsync), string.Join("-", ids.OrderBy(x => x)));
 
-            var result = descriptions
-                .Select(FillEntityProperties)
-                .ToArray();
+            var result = await _platformMemoryCache.GetOrCreateExclusiveAsync(cacheKey, async cacheEntry =>
+            {
+                using var catalogRepository = _catalogRepositoryFactory();
+
+                var descriptions = await catalogRepository
+                    .EditorialReviews
+                    .Where(x => ids.Contains(x.Id))
+                    .ToArrayAsync();
+
+                var editorialReviews = descriptions
+                    .Select(FillEntityProperties)
+                    .ToArray();
+
+                cacheEntry
+                    .AddExpirationToken(
+                        ItemCacheRegion.CreateChangeToken(
+                            editorialReviews
+                                .OfType<ExtendedEditorialReview>()
+                                .Select(x => x.ItemId)
+                                .Distinct()
+                                .ToArray()
+                        )
+                    );
+
+                return editorialReviews;
+            });
 
             return result;
         }
