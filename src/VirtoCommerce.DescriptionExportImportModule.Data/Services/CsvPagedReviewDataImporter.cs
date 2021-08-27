19d7b8912e41e77fae8d918e57d32309de861b0d
Anton Veretennikov <superiorboroda@gmail.com>
2021-08-27 13:00:26 +0300

    Implement review Importer. First step.

--- src/VirtoCommerce.DescriptionExportImportModule.Data/Services/CsvPagedReviewDataImporter.cs
+++ src/VirtoCommerce.DescriptionExportImportModule.Data/Services/CsvPagedReviewDataImporter.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentValidation;
 using VirtoCommerce.CatalogModule.Core.Model;
 using VirtoCommerce.CatalogModule.Core.Model.Search;
 using VirtoCommerce.CatalogModule.Core.Search;
+using VirtoCommerce.CatalogModule.Core.Services;
 using VirtoCommerce.DescriptionExportImportModule.Core.Models;
 using VirtoCommerce.DescriptionExportImportModule.Core.Services;
 using VirtoCommerce.Platform.Core.Assets;
@@ -14,13 +16,19 @@ namespace VirtoCommerce.DescriptionExportImportModule.Data.Services
     public class CsvPagedReviewDataImporter : CsvPagedDataImporter<CsvEditorialReview>
     {
         private readonly IProductSearchService _productSearchService;
-
+        private readonly IItemService _itemService;
+        private readonly IProductEditorialReviewService _editorialReviewService;
         public CsvPagedReviewDataImporter(IImportPagedDataSourceFactory dataSourceFactory,
             IValidator<ImportRecord<CsvEditorialReview>[]> importRecordsValidator,
             ICsvImportReporterFactory importReporterFactory, IBlobUrlResolver blobUrlResolver,
-            IProductSearchService productSearchService)
+            IProductSearchService productSearchService,
+            IItemService itemService,
+            IProductEditorialReviewService editorialReviewService)
             : base(dataSourceFactory, importRecordsValidator, importReporterFactory, blobUrlResolver)
         {
+            _itemService = itemService;
+            _productSearchService = productSearchService;
+            _editorialReviewService = editorialReviewService;
         }
 
         public override string MemberType => nameof(EditorialReview);
@@ -29,27 +37,69 @@ namespace VirtoCommerce.DescriptionExportImportModule.Data.Services
             Action<ImportProgressInfo> progressCallback, IImportPagedDataSource<CsvEditorialReview> dataSource,
             ImportErrorsContext errorsContext, ImportProgressInfo importProgress, ICsvImportReporter importReporter)
         {
-            var importReview = dataSource.Items
+            var importReviewRecords = dataSource.Items
                 // expect records that was parsed with errors
                 .Where(importContact => !errorsContext.ErrorsRows.Contains(importContact.Row))
                 .ToArray();
 
             try
             {
-                var reviewsIds = importReview.Select(x => x.Record?.Id).Distinct()
+                var reviewsIds = importReviewRecords.Select(x => x.Record?.Id).Distinct()
                     .Where(x => !string.IsNullOrEmpty(x))
                     .ToArray();
 
-                var productsSkuArray = importReview.Select(x => x.Record?.ProductSku).Distinct()
+                var existedReviews = (await _editorialReviewService.GetByIdsAsync(reviewsIds)).OfType<ExtendedEditorialReview>().ToArray();
+
+                SetIdAndSkuToNullByExistence(importReviewRecords, existedReviews);
+
+                var validationResult = await ValidateAsync(importReviewRecords, importReporter);
+
+                var invalidImportRecords = validationResult.Errors
+                    .Select(x => (x.CustomState as ImportValidationState<CsvEditorialReview>)?.InvalidRecord).Distinct().ToArray();
+
+                importReviewRecords = importReviewRecords.Except(invalidImportRecords).ToArray();
+
+                var productsSkuArray = importReviewRecords.Select(x => x.Record?.ProductSku).Distinct()
                     .Where(x => !string.IsNullOrEmpty(x))
                     .ToArray();
 
-                var productSearchResult = _productSearchService.SearchProductsAsync(
-                    new ProductSearchCriteria() { Skus = productsSkuArray, ResponseGroup = ItemResponseGroup.ItemEditorialReviews.ToString() });
+                var allProductsForSaving = new List<CatalogProduct>();
+
+                var importReviewsForAdding = importReviewRecords.Where(x => !string.IsNullOrEmpty(x.Record.ProductSku))
+                    .Select(x => x.Record).ToArray();
+
+                if (importReviewsForAdding.Length > 0)
+                {
+                    var productSearchResult = await _productSearchService.SearchProductsAsync(
+                        new ProductSearchCriteria() { Skus = productsSkuArray, ResponseGroup = ItemResponseGroup.ItemEditorialReviews.ToString() });
+
+                    var productsForReviewAdding = productSearchResult.Results;
+
+                    AddReviewsToProducts(productsForReviewAdding, importReviewsForAdding);
+
+                    allProductsForSaving.AddRange(productsForReviewAdding);
+                }
+
+                var importReviewsForUpdating = importReviewRecords.Where(x => !string.IsNullOrEmpty(x.Record.Id))
+                    .Select(x => x.Record).ToArray();
+
+                if (importReviewsForUpdating.Length > 0)
+                {
+                    var productIds = existedReviews.Select(x => x.ItemId).ToArray();
+
+                    var productSearchResult = await _productSearchService.SearchProductsAsync(
+                        new ProductSearchCriteria() { ObjectIds = productIds, ResponseGroup = ItemResponseGroup.ItemEditorialReviews.ToString() });
+
+                    var productsForReviewUpdating = productSearchResult.Results;
+
+                    PatchProductsReviews(productsForReviewUpdating, importReviewsForUpdating);
+
+                    allProductsForSaving.AddRange(productsForReviewUpdating);
+                }
 
 
-                //importProgress.CreatedCount += newContacts.Length;
-                //importProgress.UpdatedCount += existedContacts.Length;
+                importProgress.CreatedCount += importReviewsForAdding.Length;
+                importProgress.UpdatedCount += importReviewsForUpdating.Length;
             }
             catch (Exception e)
             {
@@ -62,6 +112,20 @@ namespace VirtoCommerce.DescriptionExportImportModule.Data.Services
                 importProgress.ErrorCount = importProgress.ProcessedCount - importProgress.CreatedCount -
                                             importProgress.UpdatedCount;
             }
+        }
+        private static void SetIdAndSkuToNullByExistence(ImportRecord<CsvEditorialReview>[] importReviewRecords, EditorialReview[] existedReviews)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PatchProductsReviews(IList<CatalogProduct> productsForReviewUpdating, CsvEditorialReview[] importReviewForUpdating)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AddReviewsToProducts(object productsForReviewAdding, CsvEditorialReview[] importReviewsForAdding)
+        {
+            throw new NotImplementedException();
         }
     }
 }
