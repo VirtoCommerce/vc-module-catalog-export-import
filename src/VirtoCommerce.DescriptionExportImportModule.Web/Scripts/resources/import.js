c95760bbaf73b0d4cb8dcd395af5f83e1bfddd83
Pavel Matantsev <kvissachaderah@gmail.com>
2021-08-27 13:45:21 +0300

    Add import statistics blade (not tested)

--- src/VirtoCommerce.DescriptionExportImportModule.Web/Scripts/resources/import.js
+++ src/VirtoCommerce.DescriptionExportImportModule.Web/Scripts/resources/import.js
@@ -2,6 +2,8 @@ angular.module("virtoCommerce.descriptionExportImportModule")
     .factory("virtoCommerce.descriptionExportImportModule.import", ["$resource", function ($resource) {
         return $resource("api/description/import", null, {
             validate: { method: "POST", url: "api/description/import/validate" },
+            run: { method: "POST", url: "api/description/import/run" },
+            cancel: { method: "POST", url: "api/description/import/cancel" },
         });
     },
 ]);
