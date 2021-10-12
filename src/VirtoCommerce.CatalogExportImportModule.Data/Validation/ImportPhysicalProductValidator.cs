0bdcae9b3589815cf5481e723560ac6a0817dfe3
Aleksandr Vishniakov <av@virtoway.com>
2021-10-12 08:34:07 +0200

    Fix stash apply

--- src/VirtoCommerce.CatalogExportImportModule.Data/Validation/ImportPhysicalProductValidator.cs
+++ src/VirtoCommerce.CatalogExportImportModule.Data/Validation/ImportPhysicalProductValidator.cs
@@ -1,48 +1,17 @@
-using System.Linq;
 using FluentValidation;
-using FluentValidation.Results;
-using VirtoCommerce.CatalogExportImportModule.Core;
 using VirtoCommerce.CatalogExportImportModule.Core.Models;
-using VirtoCommerce.CatalogModule.Core.Search;
 
 namespace VirtoCommerce.CatalogExportImportModule.Data.Validation
 {
     public class ImportPhysicalProductValidator : AbstractValidator<ImportRecord<CsvPhysicalProduct>[]>
     {
-        
-        private readonly IProductSearchService _productSearchService;
-
-        public ImportPhysicalProductValidator(IProductSearchService productSearchService)
+        public ImportPhysicalProductValidator()
         {
-            _productSearchService = productSearchService;
-
             AttachValidators();
         }
-
-        protected override bool PreValidate(ValidationContext<ImportRecord<CsvPhysicalProduct>[]> context, ValidationResult result)
-        {
-            var reviewTypes = GetAvailableReviewTypes();
-
-            context.RootContextData[catalogCore.ModuleConstants.Settings.General.EditorialReviewTypes.Name] = reviewTypes;
-
-            var languages = GetAvailableLanguages();
-
-            context.RootContextData[coreModuleCore.ModuleConstants.Settings.General.Languages.Name] = languages;
-
-            var skus = GetExistedSkus(context);
-
-            context.RootContextData[ModuleConstants.ContextDataSkus] = skus;
-
-            var duplicates = GetDescriptionDuplicates(context);
-
-            context.RootContextData[ModuleConstants.DuplicatedImportReview] = duplicates;
-
-            return base.PreValidate(context, result);
-        
-
         private void AttachValidators()
         {
-            RuleForEach(importRecords => importRecords).SetValidator(new ImportProductsAreNotDuplicatesValidator());
+            RuleFor(importRecords => importRecords).SetValidator(new ImportProductsAreNotDuplicatesValidator());
         }
     }
 }
